aea61723f45df8819a623cfebefa4c1887e76e21
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2024-02-16 13:28:21 +0530

    Update test_routes.py

--- api/tests/test_routes.py
+++ api/tests/test_routes.py
@@ -1,1 +1,12 @@
+import pytest
+from api.app import app
 
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_get_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    
